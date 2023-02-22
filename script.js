ab008a95e03a3b8baa804717dd99fe0faed34cff
reazaar <125664288+reazaar@users.noreply.github.com>
2023-02-22 10:59:47 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var Typed = new Typed(".multiple-text", {
+	strings: ["Web-Designer", "Graghic-Designer", "Visual-Engineer", "Lighting-Engineer", "Sound Technition", "Video-Editor", "3D-Mapping", "Stage-Designer", "Camera-man"],
+	typeSpeed: 100,
+	backSpeed: 100,
+	backDelay: 1000,
+	loop: true
+});
+
+
